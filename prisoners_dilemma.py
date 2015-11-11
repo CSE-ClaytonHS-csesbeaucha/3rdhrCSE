2eaf880e6a9e6f8c29c6ac3c8b4e8584c8b8a0fa
Claytongithub <sophiebernstein@claytonschools.net>
2015-11-11 10:04:06 -0600

    a

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -340,30 +340,14 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
 
 
 
-
-
-    ######
-    ######
-    #
     elif player == 9:
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'betray collude pattern'
         else:
-            # use history, opponent_history, score, opponent_score
-            # to compute your strategy
-            if len(opponent_history)==0: #It's the first round: collude
-                return 'c'
-            elif history[-1]=='c' and opponent_history[-1]=='b':
-                return 'b' # betray is they were severely punished last time
-            else:
-                return 'c' #otherwise collude
+            x= random.choice('b', 'c')
+            return x
 
-
-
-
-
-
-
+            
 
 
 
